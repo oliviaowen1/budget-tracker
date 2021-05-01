a778642543d0f6685a13e7d3ccd0f35fa6f902f0
Olivia Owen <olivia.owen@hotmail.com>
2021-05-01 17:25:52 +0100

    Added function to check db is online

--- public/db.js
+++ public/db.js
@@ -2,8 +2,17 @@ let db;
 // create request for new database for the budget
 const request = indexedDB.open("budget", 1);
 
-request.onupgradeneeded = function (event) {
+request.onupgradeneeded = (event) => {
   // this creates an object store called pending and it will auto increment
   const db = event.target.result;
   db.createObjectStore("pending", { autoIncrement: true });
 };
+
+request.onsuccess = (event) => {
+    db = event.target.result;
+  
+    // this will check if the app is online before it reads from the database
+    if (navigator.onLine) {
+      checkDatabase();
+    }
+  };

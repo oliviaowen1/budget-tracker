14f817490a17c4fbd479b17fc8c69802fc366c13
Olivia Owen <olivia.owen@hotmail.com>
2021-05-01 16:49:03 +0100

    Added variables for cache data types

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,7 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// Below are the files that will be cached when using our application
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
@@ -6,4 +10,6 @@ const FILES_TO_CACHE = [
     "/manifest.webmanifest",
     "/db.js"
   ];
+
+
   
